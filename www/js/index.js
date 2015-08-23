b586144be40ed1536d4ba7630ad9b7488763b59d
Alan Elnino <mail@mjniuz.com>
2015-08-23 17:08:24 +0700

    update js

--- www/js/index.js
+++ www/js/index.js
@@ -69,7 +69,7 @@ var app = {
 				break;
 			case 'message':
 				// this is the actual push notification. its format depends on the data model from the push server
-				alert('message = '+e.message+' Dari = '+e.msgcnt);
+				//alert('message = '+e.message+' Dari = '+e.msgcnt);
 				break;
 
 			case 'error':
